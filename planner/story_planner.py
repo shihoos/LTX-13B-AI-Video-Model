69ed05dd493d75757760d8ffa6ed5d878c10bbe4
Shihoos <40993398+shihoos@users.noreply.github.com>
2026-08-16 21:53:36 +0530

    Update story_planner.py

--- planner/story_planner.py
+++ planner/story_planner.py
@@ -13,10 +13,17 @@ from planner.qwen_loader import (
 
 class StoryPlanner:
 
-    def __init__(self):
+    def __init__(
+    self,
+    model=None,
+    ):
 
-        self.model = QwenStoryModel()
-
+    self.model = (
+        model
+        if model is not None
+        else QwenStoryModel()
+    )
+    
     def _read_prompt(
         self,
         filename: str,
